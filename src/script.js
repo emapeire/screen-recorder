04016007c31e6b109c04ea4bbb93f559a673ecc7
Emanuel Peire <emapeire@outlook.com>
2024-01-09 12:49:49 -0300

    Refactor screen recording functionality

--- src/script.js
+++ src/script.js
@@ -2,33 +2,36 @@ const $button = document.querySelector('button')
 let isRecording = false
 let mediaRecorder
 
-$button.addEventListener('click', async () => {
+async function startRecording() {
+  const media = await navigator.mediaDevices.getDisplayMedia({
+    video: { frameRate: { ideal: 30 } }
+  })
+  mediaRecorder = new MediaRecorder(media, {
+    mimeType: 'video/webm;codecs=vp8,opus'
+  })
+
+  mediaRecorder.addEventListener('dataavailable', (e) => {
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(e.data)
+    link.download = 'clip.webm'
+    link.click()
+  })
+
+  mediaRecorder.start()
+  isRecording = true
+  $button.textContent = '⏹️ Stop recording and download the clip'
+}
+
+function stopRecording() {
+  mediaRecorder.stop()
+  isRecording = false
+  $button.textContent = '⏺️ Start recording your screen'
+}
+
+$button.addEventListener('click', () => {
   if (!isRecording) {
-    const media = await navigator.mediaDevices.getDisplayMedia({
-      video: { frameRate: { ideal: 30 } }
-    })
-    mediaRecorder = new MediaRecorder(media, {
-      mimeType: 'video/webm;codecs=vp8,opus'
-    })
-
-    mediaRecorder.addEventListener('dataavailable', (e) => {
-      const link = document.createElement('a')
-      link.href = URL.createObjectURL(e.data)
-      link.download = 'clip.webm'
-      link.click()
-    })
-
-    mediaRecorder.start()
-    $button.textContent = '⏹️ Stop recording and save the clip'
-    isRecording = true
-
-    const [video] = media.getVideoTracks()
-    video.addEventListener('ended', () => {
-      mediaRecorder.stop()
-    })
+    startRecording()
   } else {
-    mediaRecorder.stop()
-    $button.textContent = '⏺️ Start recording your screen'
-    isRecording = false
+    stopRecording()
   }
 })
