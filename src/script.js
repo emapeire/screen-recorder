e399d6f8595fa25aec8511f659554087d1046560
Emanuel Peire <emapeire@outlook.com>
2024-01-09 13:15:41 -0300

    Refactor screen recording functionality

--- src/script.js
+++ src/script.js
@@ -1,31 +1,41 @@
 const $button = document.querySelector('button')
 let isRecording = false
 let mediaRecorder
+let mediaStream
 
 async function startRecording() {
-  const media = await navigator.mediaDevices.getDisplayMedia({
-    video: { frameRate: { ideal: 30 } }
-  })
-  mediaRecorder = new MediaRecorder(media, {
-    mimeType: 'video/webm;codecs=vp8,opus'
-  })
+  try {
+    mediaStream = await navigator.mediaDevices.getDisplayMedia({
+      video: { frameRate: { ideal: 30 } }
+    })
+    mediaRecorder = new MediaRecorder(mediaStream, {
+      mimeType: 'video/webm;codecs=vp8,opus'
+    })
 
-  mediaRecorder.addEventListener('dataavailable', (e) => {
-    const link = document.createElement('a')
-    link.href = URL.createObjectURL(e.data)
-    link.download = 'clip.webm'
-    link.click()
-  })
+    mediaRecorder.addEventListener('dataavailable', (e) => {
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(e.data)
+      link.download = 'clip.webm'
+      link.click()
+    })
 
-  mediaRecorder.start()
-  isRecording = true
-  $button.textContent = '⏹️ Stop recording and download the clip'
+    mediaRecorder.start()
+    $button.textContent = '⏹️ Stop recording and download your clip'
+    isRecording = true
+  } catch (error) {
+    console.error('Error al iniciar la grabación: ', error)
+  }
 }
 
 function stopRecording() {
-  mediaRecorder.stop()
-  isRecording = false
+  if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+    mediaRecorder.stop()
+  }
+  if (mediaStream) {
+    mediaStream.getTracks().forEach((track) => track.stop())
+  }
   $button.textContent = '⏺️ Start recording your screen'
+  isRecording = false
 }
 
 $button.addEventListener('click', () => {
